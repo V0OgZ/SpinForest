485c3b2cb41238277e2fb0a3e3c750fee508b489
v0ogz <votre-email@example.com>
2025-07-31 15:19:50 +0200

    Crypte

---
+++ spells/crypte.js
@@ -0,0 +1,6 @@
+function goBackToIndex() {
+  window.location.href = "index.html";
+}
+
+// Exemple : on pourrait charger un effet si tu veux plus tard
+console.log("Crypte active. Les murmures commencent à résonner.");
